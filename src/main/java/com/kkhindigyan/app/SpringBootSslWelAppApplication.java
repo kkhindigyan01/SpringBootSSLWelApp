49a0750defbb82c9a5dd6cbf6ded5ad898f842d8
kkhindigyan01 <kkhindigyan01@gmail.com>
2021-06-29 12:55:22 +0530

    First Commit

---
+++ src/main/java/com/kkhindigyan/app/SpringBootSslWelAppApplication.java
@@ -0,0 +1,13 @@
+package com.kkhindigyan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSslWelAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSslWelAppApplication.class, args);
+	}
+
+}
